708c2b0710b6f3ab8a1027022328ac1e9b7e8bcb
silent-knight19 <sachinsinghtomar7749@gmail.com>
2025-07-29 22:57:18 +0530

    implemented useState hook

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,18 +1,71 @@
-import React from "react";
+import { useState } from "react";
 import "../styles/login.css";
 
 const Login = () => {
-    return (    
-       <>
-       <div style={{backgroundColor: "#EAECEF", height: "100vh", width: "100%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
-       <div className="login-container">
-        <h1>Login</h1>
-        <input style={{width: "80%",height: "40px", padding: "10px", margin: "10px 0"}} type="email" placeholder="Enter your email" />
-        <input style={{width: "80%",height: "40px", padding: "10px", margin: "10px 0"}} type="password" placeholder="Enter your password" />
-        <button style={{width: "20%",height: "50px", padding: "10px", margin: "10px 0"}} type="submit">Login</button>
-       </div>
-       </div>
-       </>      
-    );
+ const [email, setEmail] = useState("");
+ const [password, setPassword] = useState("");
+ const handleSubmit = (e) => {
+    console.log("Login");
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "#EAECEF",
+          height: "100vh",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div className="login-container">
+          <h1>Login</h1>
+          <input
+          value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            style={{
+              width: "80%",
+              height: "40px",
+              padding: "10px",
+              margin: "10px 0",
+            }}
+            type="email"
+            placeholder="Enter your email"
+          />
+          <input
+          value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            style={{
+              width: "80%",
+              height: "40px",
+              padding: "10px",
+              margin: "10px 0",
+            }}
+            type="password"
+            placeholder="Enter your password"
+          />
+          <button
+            onClick={handleSubmit}
+                style={{
+              width: "20%",
+              height: "50px",
+              padding: "10px",
+              margin: "10px 0",
+            }}
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Login;
